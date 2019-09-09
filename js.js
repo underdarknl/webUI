14ac216be3a7b391328e803366c6ece8dd245f73
Kit Ex Machina <machinekit@beaglebone.localdomain>
2019-09-09 13:30:23 +0000

    Optimizing emergency stop code

--- js.js
+++ js.js
@@ -1,3 +1,20 @@
+const post = (url, data) => {
+    return fetch(url, {
+            method: "POST",
+            mode: "cors",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        })
+        .then(response => {
+            return response.json()
+        })
+        .then(data => {
+            return data;
+        })
+}
+
 const getAllAxesPosition = () => {
     const networkPromise = fetch("http://192.168.1.224:5000/get_current_position", {
             method: "get"
@@ -12,12 +29,25 @@ const getAllAxesPosition = () => {
                 axis
             } = data;
 
+            let eBtn = document.getElementById("emergency_button_status");
+            let pBtn = document.getElementById("power_button_status");
+
             for (var key in data.current_position) {
                 document.getElementById(key).innerHTML = data.current_position[key]
             }
 
-            power = (machine_power_on !== true ? "power on" : "power off")
-            document.getElementById("power_button_status").innerHTML = power;
+            if (estop_active) {
+                eBtn.innerHTML = "REMOVE EMERGENCRY STOP"
+                eBtn.setAttribute("estop", "active")
+            }
+            if (machine_power_on) {
+                pBtn.innerHTML = "power off"
+                pBtn.setAttribute("power", "off")
+            } else {
+                pBtn.innerHTML = "power on"
+                pBtn.setAttribute("power", "on")
+            }
+
         })
         .catch(function (err) {
             console.log(err)
@@ -35,8 +65,59 @@ const getAllAxesPosition = () => {
 }
 
 const emergencyBtn = (element) => {
-    console.log(element)
+    let btn = document.getElementById("emergency_button_status");
+    if (btn.getAttribute("estop")) {
+        return fetch("http://192.168.1.224:5000/set_machine_status", {
+                method: "POST",
+                mode: "cors",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    "command": "E_STOP_RESET"
+                })
+            })
+            .then(response => {
+                return response.json()
+            })
+            .then(data => {
+                if (data === 200) {
+
+                    btn.setAttribute("estop", "");
+                    btn.innerHTML = "EMERGENCRY STOP"
+
+                }
+            })
+    } else {
+        fetch("http://192.168.1.224:5000/set_machine_status", {
+                method: "POST",
+                mode: "cors",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    "command": "E_STOP"
+                })
+            })
+            .then(response => {
+                return response.json()
+            })
+            .then(data => {
+                if (data === 200) {
+                    let btn = document.getElementById("emergency_button_status");
+                    btn.setAttribute("estop", "active")
+                    btn.innerHTML = "REMOVE EMERGENCRY STOP"
+                    btn.disabled = true;
+                    setTimeout(() => {
+                        btn.disabled = false;
+                    }, 2500);
+                }
+            })
+    }
 }
 
-const emergencyStop = () => {}
+const powerBtn = (element) => {
+
+}
+
 getAllAxesPosition();
