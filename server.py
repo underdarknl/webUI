50e6b60014615d9af12b3348c5825cae8877d5f3
Jan Klopper <jan@underdark.nl>
2019-10-23 15:30:43 +0200

    use config files values

--- server.py
+++ server.py
@@ -175,5 +175,6 @@ def upload():
 
 
 if __name__ == "__main__":
-    app.run('0.0.0.0', debug=True,
-            port=5000)
+    app.run('0.0.0.0', 
+            debug=(True if config['server'].get('debug', False)),
+            port=config['server']['port'])
